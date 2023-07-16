a53f17f6da77f236947ecf0197f001aa1c995213
radbios <radbios@gmail.com>
2023-07-16 13:12:13 -0300

    order and search

--- src/pages/Client/index.js
+++ src/pages/Client/index.js
@@ -1,22 +1,20 @@
 import {React, useContext, useEffect, useState} from "react";
-import { View, TouchableOpacity, StyleSheet, Text, ScrollView, ActivityIndicator} from "react-native";
+import { View, TouchableOpacity, StyleSheet, Text, ScrollView, ActivityIndicator, TextInput} from "react-native";
 import AuthContext from "../../contexts/auth";
 import Header from "../../components/Header";
 import api from "../../services/api";
 import moment from "moment";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
     content:{
-        paddingStart: 16,
-        paddingEnd: 16,
         flex: 1,
-        paddingTop: 5,
-        paddingBottom: 5,
         backgroundColor: '#DEDBDB',
-        gap: 20
+        gap: 10,
+        paddingBottom: 5
     },
     itemList: {
         backgroundColor: 'black',
@@ -61,62 +59,130 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     clientInfo: {
+        flex: 1
+    },
+    inputContent: {
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 10,
+        height: 50,
+        flexDirection: "row"
+    },
+    search: {
+        backgroundColor: "black",
+        width: "80%",
+        borderRadius: 15,
+        color: "white",
+        paddingLeft: 20,
+        paddingRight: 20,
+        fontSize: 25
+    },
+    filter:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    scrollView: {
+        flex: 1,
+        paddingLeft: 10,
+        paddingRight: 10,
     }
 });
 
 const Client = ({navigation}) => {
-    const { user, singOut } = useContext(AuthContext);
     const [clients, setClients] = useState(null);
+    const [search, setSearch] = useState(null);
+    const [clientList, setClientList] = useState();
 
     async function getClients(){
         const response = await api.get("/clients");
         var res = response.data;
         setClients(res.data)
+        setClientList(res.data)
     }
-
     useEffect(() => {
         getClients();
+        setClientList(clients);
     }, []);
+
+    useEffect(() => {
+        if(search == ""){
+            setClientList(clients);
+        }
+        else{
+            if(clients){
+                setClientList(
+                    clients.filter(
+                        (item) =>item.name.toLowerCase().indexOf(search.toLowerCase()) > -1)
+                );
+            }
+        }
+    }, [search]);
+
+    const handleOrderClick = () => {
+        let newList = [...clientList];
+
+        newList.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
+
+        setClientList(newList);
+    }
 
     return(
         <View style={styles.container}>
             <Header  element = {navigation} />
-            {
-                clients ?
-                    <ScrollView style={styles.content}>
-                        {
-                            clients.map((client) => {
-                                return(
-                                    <TouchableOpacity key={client.id} 
-                                        onPress={ () => {
-                                            console.log(client.id)
-                                        }}
-                                        style={styles.itemList}
-                                    >
-                                        <View style={styles.clientInfo}>
-                                            <Text style={styles.textName}>
-                                                {client.name}
-                                            </Text>
-                                            <Text style={styles.textDate}>
-                                                {"Última compra:\t" + moment(client.last_purchase_date).format('DD/MM/YYYY')}
-                                            </Text>
-                                        </View>
-                                        <View style={styles.contentPrice}>
-                                            <Text style={styles.textPrice}>
-                                                {"R$: " + client.debt}
-                                            </Text>
-                                        </View>
-                                    </TouchableOpacity>
-                                    
-                                )
-                            })
-                        }
-                    </ScrollView>
-                :
-                <View style={styles.loading}>
-                    <ActivityIndicator size="large" color="#666" />
+            <View style={styles.content}>
+                <View style={styles.inputContent}>
+                    <TextInput 
+                        style={styles.search}
+                        placeholder={'Pesquisar'}
+                        onChangeText={setSearch}
+                        value={search}
+                    />
+                    <TouchableOpacity style={styles.filter} onPress={handleOrderClick}>
+                        <MaterialCommunityIcons name="order-alphabetical-ascending" size={35} color="black" />
+                    </TouchableOpacity>
                 </View>
-            }
+                {
+                    clientList ?
+                        <ScrollView style={styles.scrollView}>
+                            {
+                                clientList.map((client) => {
+                                    return(
+                                        <TouchableOpacity key={client.id} 
+                                            onPress={ () => {
+                                                console.log(client.id)
+                                            }}
+                                            style={styles.itemList}
+                                        >
+                                            <View style={styles.clientInfo}>
+                                                <Text style={styles.textName}>
+                                                    {client.name}
+                                                </Text>
+                                                <Text style={styles.textDate}>
+                                                    {"Última compra:\t"}
+                                                    {
+                                                        client.last_purchase_date ?
+                                                        moment(client.last_purchase_date).format('DD/MM/YYYY'): ""
+                                                    }
+                                                </Text>
+                                            </View>
+                                            <View style={styles.contentPrice}>
+                                                <Text style={styles.textPrice}>
+                                                    {"R$: " + client.debt}
+                                                </Text>
+                                            </View>
+                                        </TouchableOpacity>
+                                        
+                                    )
+                                })
+                            }
+                        </ScrollView>
+                    :
+                    <View style={styles.loading}>
+                        <ActivityIndicator size="large" color="#666" />
+                    </View>
+                }
+            </View>
         </View> 
     );
 }
