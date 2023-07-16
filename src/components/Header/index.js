6210adc0b56b15143a06a1bd037b9f073b4cb304
radbios <radbios@gmail.com>
2023-07-15 23:56:34 -0300

    Header custom

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -30,21 +30,71 @@ const styles = StyleSheet.create({
         color: 'white',
         fontWeight: 'bold',
         fontSize: 16
+    },
+    roleText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 10,
+        opacity: 0.7
+    },
+    moneyContent:{
+        flexDirection: "row",
+        gap: 5,
+        alignItems: "center",
+        marginLeft: 20
+    },
+    percentualContent:{
+    },
+    moneyValue:{
+        color: "white",
+        fontSize: 15,
+        fontWeight: 'bold',
+        width: 60
+    },
+    percentualMoneyValueAdmin:{
+        color: "#E12E2E",
+        opacity: 0.8
+    },
+    percentualMoneyValueSeller:{
+        color: "#36BA21",
+        opacity: 0.8
     }
 });
 
 const Header = (props) => {
-    const { user, singOut } = useContext(AuthContext);
-    
+    const { user  } = useContext(AuthContext);
     return(
         <View style={styles.container}>
             <View style={styles.content}>
                 <TouchableOpacity onPress={() => props.element.openDrawer()}>
                     <Feather name="align-justify" size={40} color="white" />
                 </TouchableOpacity>
-                <Text style={styles.text}>
-                    {'Olá, '+user.name + ' (role)'}
-                </Text>
+                <View>
+                    <Text style={styles.text}>
+                        {'Olá, '+user.name}
+                    </Text>
+                    <Text style={styles.roleText}>
+                        {"("+user.role+")"}
+                    </Text>
+                </View>
+                <View style={styles.moneyContent}>
+                    <Text style={styles.moneyValue}>
+                        {"R$:"}
+                        {user.money}
+                    </Text>
+                    <View style={styles.percentualContent}>
+                        <Text style={styles.percentualMoneyValueAdmin}>
+                            {"R$:\t"}
+                            {user.money*0.8}
+                            {"\t(80%)"}
+                        </Text>
+                        <Text style={styles.percentualMoneyValueSeller}>
+                            {"R$:\t"}
+                            {user.money*0.2}
+                            {"\t(20%)"}
+                        </Text>
+                    </View>
+                </View>
             </View>
         </View> 
     );
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -6,17 +6,29 @@ import Header from "../../components/Header";
 const styles = StyleSheet.create({
     container: {
         flex: 1
-    }
+    },
+    content:{
+        backgroundColor: "#DEDBDB",
+        flex: 1,
+        paddingTop: 30,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        paddingRight: 10
+    },
 });
 
 const Dashboard = ({navigation}) => {
     const {  user, singOut } = useContext(AuthContext);
-    
+    console.log(user)
     
 
     return(
         <View style={styles.container}>
             <Header  element = {navigation} />
+            <View style={styles.content}>
+               
+
+            </View>
         </View> 
     );
 }
